29a0507475840cb312095ab823b60060f5da627d
desktop-gdy <rhdek228@gmail.com>
2025-01-23 17:22:44 +0900

    리스트/등록 view,controller 추가

--- src/main/java/org/zerock/w1/todo/TodoListController.java
+++ src/main/java/org/zerock/w1/todo/TodoListController.java
@@ -1,5 +1,22 @@
 package org.zerock.w1.todo;
 
-@We
-public class TodoListController {
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(name = "todoListController", urlPatterns = "/todo/list")
+public class TodoListController extends HttpServlet{
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		System.out.println("/todo/list");
+		
+		req.getRequestDispatcher("/WEB-INF/todo/list.jsp").forward(req, resp);
+	}
+	
 }
